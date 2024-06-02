174e94dd11761949e48c907aafb01e5503ffe81c
Kalil <kalilgarcia38@gmail.com>
2024-06-02 11:22:57 -0300

    a

---
+++ app/ExpoGo/src/app/(user-app)/(tabs)/_layout.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { MaterialIcons, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View } from "react-native";
+import GradientBackground from '../../components/gradient';
+
+
+export default function TabRoutesLayout() {
+    return (
+        <Tabs screenOptions={{
+            tabBarInactiveTintColor: '#fff',
+            tabBarHideOnKeyboard: true,
+            tabBarActiveTintColor: '#fff',
+            tabBarBackground: () => (
+                <GradientBackground ></GradientBackground>
+            ),
+        }}>
+
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ size, color, focused = false }) => {
+                        return focused == false ? <Ionicons name="home-outline" size={28} color={color} /> :
+                            <Ionicons name="home" size={32} color={color} />;
+                    },
+                    tabBarShowLabel: false,
+                    headerShown: false,
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{ title: "Pesquisar", tabBarIcon: ({ size, color, focused }) => { return focused == false ? <Ionicons name="search-outline" size={28} color={color} /> : <Ionicons name="search" size={34} color={color} />; }, headerShown: false, tabBarShowLabel: false }} />
+            <Tabs.Screen
+                name="liked"
+                options={{ title: "Favoritos", tabBarIcon: ({ size, color, focused }) => { return focused == false ? <MaterialIcons name="favorite-outline" size={28} color={color} /> : <MaterialIcons name="favorite" size={34} color={color} /> }, headerShown: false, tabBarShowLabel: false }} />
+            <Tabs.Screen
+                name="[profile]"
+                options={{
+                    title: "",
+                    headerTitleAlign: 'center',
+                    headerTintColor: '#fff',
+                    tabBarShowLabel: false,
+                    headerStyle: {
+                        backgroundColor: '#ABC6FD', // Defina a cor que você deseja aqui
+                        elevation: 0,
+                    },
+                    tabBarIcon: ({ size, color, focused }) => {
+                        return focused == false ? <Ionicons name="person-outline" size={28} color={color} /> :
+                            <Ionicons name="person" size={32} color={color} />
+                    },
+                }}
+            />
+
+        </Tabs>
+
+    );
+}
---
+++ app/ExpoGo/src/app/(user-app)/_layout.js
@@ -0,0 +1,114 @@
+import { Stack, Redirect, Stack} from 'expo-router';
+import GradientBackground from '../../components/gradient';
+import { Text } from 'react-native';
+import React from 'react';
+
+import { useSession } from '../../contexts/authContext.js';
+
+
+export default function AppLayout() {
+  const { session, isLoading } = useSession();
+
+  // You can keep the splash screen open, or render a loading screen like we do here.
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
+
+  // Only require authentication within the (app) group's layout as users
+  // need to be able to access the (auth) group and sign in again.
+  if (!session) {
+    // On web, static rendering will stop here as the user is not authenticated
+    // in the headless Node process that the pages are rendered in.
+    return <Redirect href="/sign-in" />;
+  }
+  return (
+    <Stack screenOptions={{
+      headerTintColor: '#fff', //  isso define a cor do botão de voltar e do título para branco
+      headerShadowVisible: false,
+    }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="register-client" options={{ headerShown: false }} />
+      <Stack.Screen name="SuperMarkets" options={{headerShown: false}} />
+      <Stack.Screen name="login" options={{headerShown: false}} />
+
+      <Stack.Screen name="promotion" options={{
+        title: "promoção",
+        headerBackground: () => (
+          <GradientBackground ></GradientBackground>
+        ),
+      }} />
+      <Stack.Screen name="notification" options={{
+        title: "Notificações",
+        headerBackground: () => (
+          <GradientBackground ></GradientBackground>
+        ),
+      }} />
+      <Stack.Screen name="store-profile" options={{
+        title: "perfil do mercado",
+        headerTransparent: true,
+      }} />    
+      <Stack.Screen
+        name="edit-location"
+        options={{
+          elevation: 0,
+          title: "Editar Localização",
+          headerStyle: {
+            backgroundColor: '#ABC6FD', // Defina a cor que você deseja aqui
+            // Remova a sombra no Android
+            shadowOpacity: 0, // Remova a sombra no iOS
+
+          },
+        }} />
+      <Stack.Screen
+        name="edit-profile"
+        options={{
+          elevation: 0,
+          title: "Editar perfil",
+          headerStyle: {
+            backgroundColor: '#ABC6FD', // Defina a cor que você deseja aqui
+            // Remova a sombra no Android
+            shadowOpacity: 0, // Remova a sombra no iOS
+
+          },
+        }}
+      />
+      <Stack.Screen
+        name="configuration"
+        options={{
+          title: "configurações",
+          headerTransparent: true, // Torna o cabeçalho transparente
+        }}
+      />
+      <Stack.Screen
+        name="about-us"
+        options={{
+          title: "Sobre o achei barato",
+          headerTransparent: true, // Torna o cabeçalho transparente
+        }} />
+      <Stack.Screen
+        name="acount"
+        options={{
+          title: "conta",
+          headerTransparent: true, // Torna o cabeçalho transparente
+        }} />
+      <Stack.Screen
+        name="notificationsConfig"
+        options={{
+          title: "notificações",
+          headerTransparent: true, // Torna o cabeçalho transparente
+        }} />
+      <Stack.Screen
+        name="location"
+        options={{
+          title: "Localização",
+          headerTransparent: true,
+        }} />
+        <Stack.Screen
+        name="ProductsAndPromotions"
+        options={{
+          title: "Produtos",
+          headerTransparent: true,
+        }} />
+    </Stack>
+  );
+}
